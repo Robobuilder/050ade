8d220717d41481b283a28881a2806f330db5ce14
Robobuilder <daniel.kt.hilton@gmail.com>
2013-05-07 22:42:39 +0200

    changed to ondisk.

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -210,7 +210,7 @@ static void pagefault(unsigned virt_page)
 
 	unsigned swapPage;
 	/* Check weather the pagetable entry is in memory */
-	if(!pte->inmemory){
+	if(pte->ondisk){ //inmemory not working as i think?!
 		swapPage = pte->page;
 	} else{
 		swapPage = new_swap_page();
